a391327786c77ea0536fdfcea9f0ccc9ebd5f283
Raman <ramansuryawanshi364@gmail.com>
2025-11-15 00:28:07 +0530

    wrote script to clean old logs

--- clean_logs.sh
+++ clean_logs.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-
+#simple log cleaner to clean the old logs automatically
 LOG_DIR="var/log/myapp"
-DAY=7
+DAYS=7
+
+find "$LOG_DIR" -type f -mtime +$DAYS -exec rm -f {} \;
+
+echo "Logs older than $DAYS days are deleted."
 
